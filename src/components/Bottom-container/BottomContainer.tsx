395838f3d961b28450f91ebeee5ed6c7de59888c
abhi-psoft <abhisheksuryawanshi@prissoft.net>
2023-05-01 21:58:37 +0530

    Code sanity

---
+++ src/components/Bottom-container/BottomContainer.tsx
@@ -0,0 +1,13 @@
+import './BottomContainer.css'
+
+function BottomContainer() {
+    return (
+        <main className="bottom-caontainer">
+            <section className='px-4'>
+                <h1> Hello world</h1>
+            </section>
+        </main>
+    );
+}
+
+export default BottomContainer;
