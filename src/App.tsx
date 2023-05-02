ffee66e3715d9bf5e1c8f53f247ccc0b2fa2355b
abhi-psoft <abhisheksuryawanshi@prissoft.net>
2023-05-02 14:36:48 +0530

    Added default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Banner from "./components/Banner";
 import './App.css'
 import Timeline from "./components/Timeline/Timeline";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import CoverLetter from "./components/CoverLetter/CoverLetter";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Banner/>}/>
+        <Route path="*" element={<Navigate to ="/react-profile" />}/>
+        <Route path="/react-profile" element={<Banner/>}/>
         <Route path="/cover-letter" element={<CoverLetter/>}/>
     </Routes>
     
